16e24a8c7cc97dd63a91cc4e8f272297e30666b3
Piet Geursen <pietgeursen@gmail.com>
2017-08-22 12:02:40 +1200

    dunno

--- inc/state_machine.hpp
+++ inc/state_machine.hpp
@@ -1,7 +1,25 @@
+#include <boost/statechart/event.hpp>
 #include <boost/statechart/state_machine.hpp>
 #include <boost/statechart/simple_state.hpp>
 
-
 namespace sc = boost::statechart;
 
-struct EvStartStop : sc::event< EvStartStop > {};
+//forward declarations
+struct Active;
+struct Disconnected;
+
+struct Mask : sc::state_machine< Mask, Active > {};
+
+struct EvStartPairing : sc::event< EvStartPairing > {};
+struct EvVolumeUp : sc::event< EvVolumeUp > {};
+struct EvVolumeDown : sc::event< EvVolumeDown > {};
+struct EvNextTrack : sc::event< EvNextTrack > {};
+struct EvPreviousTrack : sc::event< EvPreviousTrack > {};
+
+
+struct Active : sc::simple_state<
+  Active, Mask, Disconnected > {};
+
+struct Disconnected : sc::simple_state< Disconnected, Active > {};
+
+
--- test.cpp
+++ test.cpp
@@ -1,5 +1,8 @@
 #include "gtest/gtest.h"
+#include "state_machine.hpp"
 
 TEST(FactorialTest, HandlesZeroInput) {
+  Mask myMask; 
+  myMask.initiate();
   EXPECT_EQ(1, 1);
 }
