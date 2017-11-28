7fa0a64349488d5b9f40787f7fe49fd866e6515d
Piet Geursen <pietgeursen@gmail.com>
2017-11-28 22:14:37 +1300

    Adds skeleton of pointers exercise

---
+++ pointers.cpp
@@ -0,0 +1,29 @@
+#include <stdint.h> //gives uint8_t etc
+#include <cstdlib> //gives malloc + free
+#include <stdio.h> //give printf
+
+//function by copy
+//
+//function by ref
+//
+
+//function to create an array of booleans (think a row of conways cells) 
+//function takes an integer for the size of the array and returns a pointer to the array it created
+//We're going to need to use malloc for this. But try it first and see if it works without. If you do this without malloc, your array will be on the stack, not the heap.
+//Why do we need to use the heap not the stack here? Do some googling and see if you can explain it in your own words.
+
+
+// extra for experts:
+// Pointer arithmetic and indexes
+int main(){
+  printf("%u\n", 1);
+
+  //create an int.
+  //
+  //create a pointer to an int
+  //
+  //modify the int through the pointer
+  //
+
+  
+}
